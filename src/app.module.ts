3655d3ece9c48b6e88ca64fd55a2a1a8260b9e03
Paolo Catalani <p.cata85@gmail.com>
2020-06-02 13:59:07 +0200

    add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
